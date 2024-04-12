eb9e23efa18829c4ebd0e5279f35b7097ccc2cd2
Ancishan <shantodev027@gmail.com>
2024-04-13 00:30:58 +0600

    [Updata] banner section

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
 
     return (
         <div className="navbar drop-shadow-xl text-green-500 font-bold mt-3" data-aos="fade-left" data-aos-duration="3000" data-aos-delay="500">
-            <div className="navbar-start">
+            <div className="navbar-start pl-5">
                 <div className="dropdown z-[50]">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
