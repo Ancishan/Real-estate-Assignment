87a07606ff9c18158efa5bf744f9fb0168055358
Ancishan <shantodev027@gmail.com>
2024-04-12 23:18:12 +0600

    [Fixed] Navbar Dropdown

--- src/Pages/BannerSec.jsx
+++ src/Pages/BannerSec.jsx
@@ -24,8 +24,8 @@ const BannerSec = () => {
     }, [triggerAnimation]);
 
     return (
-        <div ref={ref} className="carousel  pt-30  h-[70vh] mt-4 mx-auto ml-3 mr-3 font-poppins md:relative">
-            <div id="slide1" className={`carousel-item relative w-full ${triggerAnimation ? 'aos-animate' : ''}`} data-aos="zoom-in" data-aos-duration="2000" data-aos-delay="200">
+        <div ref={ref} className="carousel z-[1] pt-30  h-[70vh] mt-4 mx-auto ml-3 mr-3 font-poppins md:relative">
+            <div id="slide1" className={` z-[1] carousel-item relative w-full ${triggerAnimation ? 'aos-animate' : ''}`} data-aos="zoom-in" data-aos-duration="2000" data-aos-delay="200">
                 <img src={img1} className="w-full h-full object-cover object-center" alt="Slide 1" />
                 <div className='absolute top-1/3 left-1/3 transform -translate-x-1/2 -translate-y-1/2' data-aos="fade-up" data-aos-duration="1000" data-aos-delay="800">
                     <h2 className='text-3xl font-bold md:font-bold md:text-6xl text-green-500 ' data-aos="fade-right" data-aos-duration="3000" data-aos-delay="500" >Welcome <br /> To  <br /> Pranti Agro Farm</h2>
@@ -35,7 +35,7 @@ const BannerSec = () => {
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide2" className={`carousel-item relative w-full ${triggerAnimation ? 'aos-animate' : ''}`} data-aos="fade-right" data-aos-duration="2000"  data-aos-delay="500">
+            <div id="slide2" className={`z-[1] carousel-item relative w-full ${triggerAnimation ? 'aos-animate' : ''}`} data-aos="fade-right" data-aos-duration="2000"  data-aos-delay="500">
                 <img src={img2} className="w-full h-full object-cover object-center" />
                 <div className='absolute top-1/3 left-1/3 transform -translate-x-1/2 -translate-y-1/2' data-aos="fade-up" data-aos-duration="1000" data-aos-delay="800">
                     <h2 className='font-extrabold text-6xl text-green-500 '>Welcome <br /> To  <br /> Pranti Agro Farm</h2>
@@ -45,7 +45,7 @@ const BannerSec = () => {
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide3" className={`carousel-item relative w-full ${triggerAnimation ? 'aos-animate' : ''}`} data-aos="zoom-in" data-aos-duration="2000" data-aos-delay="700">
+            <div id="slide3" className={`z-[1] carousel-item relative w-full ${triggerAnimation ? 'aos-animate' : ''}`} data-aos="zoom-in" data-aos-duration="2000" data-aos-delay="700">
                 <img src={img3} className="w-full h-full object-cover object-center" />
                 <div className='absolute top-1/3 left-1/3 transform -translate-x-1/2 -translate-y-1/2' data-aos="fade-up" data-aos-duration="1000" data-aos-delay="800">
                     <h2 className='font-extrabold text-6xl text-green-500 '>Welcome <br /> To  <br /> Pranti Agro Farm</h2>
@@ -55,7 +55,7 @@ const BannerSec = () => {
                     <a href="#slide4" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide4" className={`carousel-item relative w-full ${triggerAnimation ? 'aos-animate' : ''}`} data-aos="zoom-in" data-aos-duration="2000" data-aos-delay="1000">
+            <div id="slide4" className={` z-[1] carousel-item relative w-full ${triggerAnimation ? 'aos-animate' : ''}`} data-aos="zoom-in" data-aos-duration="2000" data-aos-delay="1000">
                 <img src={img4} className="w-full h-full object-cover object-center" />
                 <div className='absolute top-1/3 left-1/3 transform -translate-x-1/2 -translate-y-1/2' data-aos="fade-up" data-aos-duration="1000" data-aos-delay="800">
                     <h2 className='font-extrabold text-6xl text-green-500 '>Welcome <br /> To  <br /> Pranti Agro Farm</h2>
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -28,13 +28,13 @@ const Navbar = () => {
     );
 
     return (
-        <div className="navbar drop-shadow-xl text-green-500 font-bold mt-3" data-aos="fade-left" data-aos-duration="3000" data-aos-delay="500">
+        <div className="navbar drop-shadow-xl text-green-500 font-bold mt-3 relative z-50" data-aos="fade-left" data-aos-duration="3000" data-aos-delay="500">
             <div className="navbar-start">
-                <div className="dropdown">
+                <div className="dropdown ">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-2 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 absolute rounded-box w-40">
                         {navLinks}
                     </ul>
                 </div>
